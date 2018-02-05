3c7cf6617ab66d0b76f1dda24786bea9dcbb1fea
Naveed tejani <naveed.tejani@outlook.com>
2018-02-06 00:59:00 +0500

    Added solutions and a web project on asp.net core

---
+++ frontend/AMS.frontend/AMS.frontend.web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AMS.frontend.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
