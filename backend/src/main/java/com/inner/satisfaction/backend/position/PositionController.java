a6856476887a82da252707a72b2baa55f03ebe24
Mohsin Mansoor Kerai <mohsin.kerai@gmail.com>
2018-02-20 01:12:47 +0500

    Added More Data - Some Entries Left

--- backend/src/main/java/com/inner/satisfaction/backend/position/PositionController.java
+++ backend/src/main/java/com/inner/satisfaction/backend/position/PositionController.java
@@ -1,14 +1,13 @@
 package com.inner.satisfaction.backend.position;
 
 import static com.inner.satisfaction.backend.base.BaseController.PREFIX;
-import static com.inner.satisfaction.backend.position.PositionController.PATH;
 
 import com.inner.satisfaction.backend.base.BaseController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(PREFIX + PATH)
+@RequestMapping(PREFIX + PositionController.PATH)
 public class PositionController extends BaseController<Position> {
 
   public static final String PATH = "position";
--- backend/src/main/java/com/inner/satisfaction/backend/position/active/PositionOnActiveLevelController.java
+++ backend/src/main/java/com/inner/satisfaction/backend/position/active/PositionOnActiveLevelController.java
@@ -1,19 +1,19 @@
 package com.inner.satisfaction.backend.position.active;
 
 import static com.inner.satisfaction.backend.base.BaseController.PREFIX;
-import static com.inner.satisfaction.backend.position.PositionController.PATH;
 
 import com.inner.satisfaction.backend.base.BaseController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(PREFIX + PATH)
+@RequestMapping(PREFIX + PositionOnActiveLevelController.PATH)
 public class PositionOnActiveLevelController extends BaseController<PositionOnActiveLevel> {
 
   public static final String PATH = "position/level/active";
 
-  public PositionOnActiveLevelController(PositionOnActiveLevelService positionOnActiveLevelService) {
+  public PositionOnActiveLevelController(
+      PositionOnActiveLevelService positionOnActiveLevelService) {
     super(positionOnActiveLevelService);
   }
 }
