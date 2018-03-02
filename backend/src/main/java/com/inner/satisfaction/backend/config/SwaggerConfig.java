9e0d1d3473793461ce6c2be1a449c5dd2167a50c
Mohsin Mansoor Kerai <mohsin.kerai@gmail.com>
2018-03-02 23:46:30 +0500

    Integrated Swagger

---
+++ backend/src/main/java/com/inner/satisfaction/backend/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.inner.satisfaction.backend.config;
+
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+import com.google.common.base.Predicate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+  @Bean
+  public Docket productApi() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .apiInfo(apiInfo())
+        .select()
+        .apis(RequestHandlerSelectors.any())
+        .paths(PathSelectors.any())
+        .build();
+  }
+
+  private ApiInfo apiInfo() {
+    return new ApiInfoBuilder().title("Inner-Satisfaction Backend")
+        .description("Description of Swagger")
+        .build();
+  }
+}
