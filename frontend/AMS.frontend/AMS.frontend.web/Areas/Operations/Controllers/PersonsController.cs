5764ad003c98b99720ea6ec8c6b071386e95345b
1312152 <saifaliprasala.95@gmail.com>
2018-03-17 05:40:43 +0500

    Integrated salutation API

--- frontend/AMS.frontend/AMS.frontend.web/Areas/Operations/Controllers/PersonsController.cs
+++ frontend/AMS.frontend/AMS.frontend.web/Areas/Operations/Controllers/PersonsController.cs
@@ -6,6 +6,12 @@ using Microsoft.Extensions.Options;
 using System.Threading.Tasks;
 using AMS.frontend.web.Areas.Operations.Models.Persons;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Net.Http;
+using System;
+using System.Net.Http.Headers;
+using AMS.frontend.web.Areas.Operations.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace AMS.frontend.web.Areas.Operations.Controllers
 {
@@ -15,6 +21,8 @@ namespace AMS.frontend.web.Areas.Operations.Controllers
         private readonly IMapper _mapper;
         private readonly Configuration _configuration;
 
+        string Baseurl = "http://13.93.85.18:8080/constants/";
+        
         public PersonsController(IMapper mapper, IOptions<Configuration> configuration)
         {
             this._mapper = mapper;
@@ -28,6 +36,33 @@ namespace AMS.frontend.web.Areas.Operations.Controllers
 
         public async Task<IActionResult> Add()
         {
+            //RestfulClient restfulClient = new RestfulClient();
+
+            using (var client = new HttpClient()) {
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage Res = await client.GetAsync("salutatuions");
+
+                if (Res.IsSuccessStatusCode)
+                {
+                    var result = await Res.Content.ReadAsStringAsync();
+                    dynamic myObject = JArray.Parse(result);
+                    var list = new List<SelectListItem>();
+                    
+                    foreach (var item in myObject) {
+                        var id = Convert.ToString(item.id);
+                        var salutation = Convert.ToString(item.salutation);
+                        list.Add(new SelectListItem { Text = salutation, Value = id });
+                    }
+
+
+                    ViewBag.SalutationList = list;
+                }
+            
+            }
+
+            //var getSalutation = restfulClient.getSalutation();
+
             return View();
         }
 
---
+++ frontend/AMS.frontend/AMS.frontend.web/Areas/Operations/Models/RestfulClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace AMS.frontend.web.Areas.Operations.Models
+{
+    public class RestfulClient
+    {
+        private HttpClient client;
+        private static string BASE_URL = "http://13.93.85.18:8080/constants/";
+
+        public async Task<string> getSalutation()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri(BASE_URL);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            try {
+                HttpResponseMessage Res = await client.GetAsync("salutatuions");
+                if (Res.IsSuccessStatusCode)
+                {
+                    return Res.Content.ReadAsStringAsync().Result.ToString();
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            return null;
+        }
+    }
+}
