6b366d6a17e6cd0a5178ba88f34b3fc3bf57796a
Mohsin Mansoor Kerai <mohsin.kerai@gmail.com>
2018-05-05 22:36:51 +0500

    FindByCnicWorking

--- backend/src/main/java/com/inner/satisfaction/backend/person/PersonController.java
+++ backend/src/main/java/com/inner/satisfaction/backend/person/PersonController.java
@@ -6,6 +6,7 @@ import com.inner.satisfaction.backend.base.BaseController;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,14 +23,14 @@ public class PersonController extends BaseController<Person> {
     this.personService = personService;
   }
 
-  @RequestMapping("/search/cnic")
   @ResponseStatus(HttpStatus.OK)
-  public Person findByCnic(@RequestParam("value") String cnic) {
+  @RequestMapping(value = "/search/cnic", method = RequestMethod.GET)
+  public Person findByCnic(@RequestParam("cnic") String cnic) {
     return personService.findByCnic(cnic);
   }
 
   @ResponseStatus(HttpStatus.OK)
-  @RequestMapping("/search/findByCnicOrFirstNameOrLastName")
+  @RequestMapping(value = "/search/findByCnicOrFirstNameOrLastName", method = RequestMethod.GET)
   public List<Person> findByCnicOrFirstNameOrLastName(
     @RequestParam("cnic") String cnic,
     @RequestParam("firstName") String firstName,
