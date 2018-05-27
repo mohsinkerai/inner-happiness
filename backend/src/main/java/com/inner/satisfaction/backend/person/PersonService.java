4da3cf0e766935cc2b0c8627fa707e8ecc2acd58
Mohsin Mansoor Kerai <mohsin.kerai@gmail.com>
2018-05-27 19:17:35 +0500

    Person Relation Person Added

--- backend/src/main/java/com/inner/satisfaction/backend/person/PersonService.java
+++ backend/src/main/java/com/inner/satisfaction/backend/person/PersonService.java
@@ -1,16 +1,16 @@
 package com.inner.satisfaction.backend.person;
 
-import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 import com.inner.satisfaction.backend.base.BaseService;
 import com.inner.satisfaction.backend.person.dto.ReducedPersonDto;
 import com.inner.satisfaction.backend.person.relation.PersonRelationPerson;
 import com.inner.satisfaction.backend.person.relation.PersonRelationPersonService;
 import com.inner.satisfaction.backend.utils.DtoEntityConverter;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import javax.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,20 +19,16 @@ public class PersonService extends BaseService<Person> {
   private final DtoEntityConverter<ReducedPersonDto, Person> dtoConverter;
   private final PersonRelationPersonService personRelationPersonService;
   private final PersonRepository personRepository;
-  private final String path;
 
   protected PersonService(
     PersonRepository personRepository,
     PersonValidation personValidation,
     DtoEntityConverter<ReducedPersonDto, Person> dtoConverter,
-    PersonRelationPersonService personRelationPersonService,
-    @Value("${person.images.path}")
-      String path) {
+    PersonRelationPersonService personRelationPersonService) {
     super(personRepository, personValidation);
     this.personRepository = personRepository;
     this.dtoConverter = dtoConverter;
     this.personRelationPersonService = personRelationPersonService;
-    this.path = path;
   }
 
   public Person findByCnic(String cnic) {
@@ -46,17 +42,44 @@ public class PersonService extends BaseService<Person> {
         cnic, firstName, lastName);
   }
 
+  /**
+   * 1. Convert all new to persons
+   * 2. Create relations (don't save)
+   * 3. Find All Relations of personIdOne.
+   * 4. Remove Extra Relations (that've ended) and Create New where needed.
+   */
   @Override
   @Transactional
   public Person save(Person person) {
-    List<ReducedPersonDto> reducedPersons = Optional.ofNullable(person.getFamilyRelations())
-      .orElse(ImmutableList.of());
-    Person savedPerson = super.save(person);
-    List<ReducedPersonDto> updatedReducedPersons = reducedPersons.stream()
-      .map(rp -> updateReducedPerson(rp, savedPerson.getId())).collect(
-        Collectors.toList());
-    savedPerson.setFamilyRelations(updatedReducedPersons);
-    return savedPerson;
+    List<ReducedPersonDto> familyRelations = person.getFamilyRelations();
+    person = super.save(person);
+
+    List<ReducedPersonDto> reducedPersonsWithId = familyRelations
+      .stream()
+      .map(this::attachPersonId)
+      .collect(Collectors.toList());
+
+    List<ReducedPersonDto> personRelations = findAllRelations(person.getId());
+    managePRP(reducedPersonsWithId, personRelations, person.getId());
+
+    person.setFamilyRelations(familyRelations);
+    return person;
+  }
+
+  private void managePRP(List<ReducedPersonDto> reducedPersonsWithId,
+    List<ReducedPersonDto> personRelations, long personId) {
+    for(ReducedPersonDto reducedPersonDto : personRelations){
+      personRelationPersonService.remove(personId, reducedPersonDto.getId());
+    }
+    for(ReducedPersonDto reducedPersonDto : reducedPersonsWithId){
+      personRelationPersonService.save(
+        PersonRelationPerson.builder()
+        .firstPersonId(personId)
+        .secondPersonId(reducedPersonDto.getId())
+        .relation(reducedPersonDto.getRelation())
+        .build()
+      );
+    }
   }
 
   @Override
@@ -65,37 +88,35 @@ public class PersonService extends BaseService<Person> {
     if (one == null) {
       return null;
     }
-    List<ReducedPersonDto> reducedPersons = personRelationPersonService
-      .findByFirstPersonId(id)
-      .stream()
-      .map(prp -> findOneReducedPerson(prp, id))
-      .collect(Collectors.toList());
+    List<ReducedPersonDto> reducedPersons = findAllRelations(id);
     one.setFamilyRelations(reducedPersons);
     return one;
   }
 
-  private ReducedPersonDto findOneReducedPerson(PersonRelationPerson prp, Long id) {
+  private List<ReducedPersonDto> findAllRelations(long id) {
+    return personRelationPersonService
+      .findByFirstPersonId(id)
+      .stream()
+      .map(this::findOneReducedPerson)
+      .collect(Collectors.toList());
+  }
+
+  private ReducedPersonDto findOneReducedPerson(PersonRelationPerson prp) {
     Person second = super.findOne(prp.getSecondPersonId());
     ReducedPersonDto reducedPersonDto = dtoConverter.convertTo(second);
     reducedPersonDto.setRelation(prp.getRelation());
     return reducedPersonDto;
   }
 
-  private ReducedPersonDto updateReducedPerson(ReducedPersonDto reducedPerson, long savedPersonId) {
-    if (reducedPerson.getId() == null) {
-      Person relativePerson = dtoConverter.convertFrom(reducedPerson);
-      ReducedPersonDto savedReducedPerson = dtoConverter
-        .convertTo(personRepository.save(relativePerson));
-      savedReducedPerson.setRelation(reducedPerson.getRelation());
-      reducedPerson = savedReducedPerson;
+  private ReducedPersonDto attachPersonId(ReducedPersonDto reducedPersonDto) {
+    Person person = findByCnic(reducedPersonDto.getCnic());
+    if(person != null) {
+      reducedPersonDto.setId(person.getId());
+      return reducedPersonDto;
     }
-
-    PersonRelationPerson prp = PersonRelationPerson.builder()
-      .firstPersonId(savedPersonId)
-      .secondPersonId(reducedPerson.getId())
-      .relation(reducedPerson.getRelation())
-      .build();
-    personRelationPersonService.save(prp);
-    return reducedPerson;
+    person = dtoConverter.convertFrom(reducedPersonDto);
+    person = personRepository.save(person);
+    reducedPersonDto.setId(person.getId());
+    return reducedPersonDto;
   }
 }
