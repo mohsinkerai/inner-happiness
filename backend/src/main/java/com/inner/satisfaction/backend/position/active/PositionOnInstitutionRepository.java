afbda47408e409690efc954394852a586f617f6b
Mohsin Mansoor Kerai <mohsin.kerai@gmail.com>
2018-08-18 03:41:18 +0500

    Adding Position Cycle Insitution API's - Maybe Fragile

--- backend/src/main/java/com/inner/satisfaction/backend/position/active/PositionOnInstitutionRepository.java
+++ backend/src/main/java/com/inner/satisfaction/backend/position/active/PositionOnInstitutionRepository.java
@@ -1,9 +1,11 @@
 package com.inner.satisfaction.backend.position.active;
 
 import com.inner.satisfaction.backend.base.BaseRepository;
+import java.util.List;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PositionOnInstitutionRepository extends BaseRepository<PositionOnInstitution> {
 
+  List<PositionOnInstitution> findByInstitutionId(long institutionId);
 }
