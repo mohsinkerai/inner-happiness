b6f6f304b28cc5990641badd7bc9397da3cf5f6f
Mohsin Mansoor Kerai <mohsin.kerai@gmail.com>
2018-09-19 00:56:35 +0500

    Deprecating HighestLevelOFStudyController

--- backend/src/main/java/com/inner/satisfaction/backend/lookups/highestLevelStudy/HighestLevelOfStudyController.java
+++ backend/src/main/java/com/inner/satisfaction/backend/lookups/highestLevelStudy/HighestLevelOfStudyController.java
@@ -7,8 +7,8 @@ import com.inner.satisfaction.backend.base.BaseController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping(CONSTANTS_PREFIX + PATH)
+//@RestController
+//@RequestMapping(CONSTANTS_PREFIX + PATH)
 public class HighestLevelOfStudyController extends BaseController<HighestLevelOfStudy> {
 
   public static final String PATH = "highest-level-of-study";
