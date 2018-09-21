d18732647a7dadb848a6b2bfd4bf87b7a566a6e3
Mohsin Mansoor Kerai <mohsin.kerai@gmail.com>
2018-09-21 12:48:28 +0500

    Making jamatkhana regional and local nullable at DB

--- backend/src/main/java/com/inner/satisfaction/backend/person/PersonValidation.java
+++ backend/src/main/java/com/inner/satisfaction/backend/person/PersonValidation.java
@@ -3,13 +3,17 @@ package com.inner.satisfaction.backend.person;
 import com.inner.satisfaction.backend.base.BaseEntityValidation;
 import com.inner.satisfaction.backend.base.MyValidationException;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 
 @Component
 public class PersonValidation implements BaseEntityValidation<Person> {
 
   @Override
   public void isValidToSave(Person entity) throws MyValidationException {
-
+    Assert.notNull(entity, "Person Entity should not be null");
+    Assert.notNull(entity.getJamatkhana(), "Person jamatkhana should not be null");
+    Assert.notNull(entity.getLocalCouncil(), "Person local council should not be null");
+    Assert.notNull(entity.getRegionalCouncil(), "Person regional council should not be null");
   }
 
   @Override
