af1e84e503f8bf56bc751cae3d125c535210fc30
Mohsin Mansoor Kerai <mohsin.kerai@gmail.com>
2018-09-22 23:24:34 +0500

    Working Test

--- backend/src/test/java/com/inner/satisfaction/backend/base/MyTestExecutionListener.java
+++ backend/src/test/java/com/inner/satisfaction/backend/base/MyTestExecutionListener.java
@@ -4,6 +4,7 @@ import static com.wix.mysql.EmbeddedMysql.anEmbeddedMysql;
 import static com.wix.mysql.ScriptResolver.classPathScript;
 import static com.wix.mysql.config.Charset.UTF8;
 import static com.wix.mysql.distribution.Version.v5_6_23;
+import static com.wix.mysql.distribution.Version.v5_7_19;
 
 import com.wix.mysql.EmbeddedMysql;
 import com.wix.mysql.config.MysqldConfig;
@@ -25,9 +26,9 @@ public class MyTestExecutionListener {
   private EmbeddedMysql mysqld;
 
   public void setUp() {
-    MysqldConfig config = MysqldConfig.aMysqldConfig(v5_6_23)
+    MysqldConfig config = MysqldConfig.aMysqldConfig(v5_7_19)
         .withCharset(UTF8)
-        .withPort(3301)
+        .withPort(3302)
         .withTimeZone(TimeZone.getDefault())
         .withTempDir(System.getProperty("java.io.tmpdir"))
         .build();
