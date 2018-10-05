e99ff875518caa849a9a40004a3cb26566f152c0
Mohsin Mansoor Kerai <mohsin.kerai@gmail.com>
2018-10-05 14:19:44 +0500

    SSH Key

--- backend/deploy-qa.sh
+++ backend/deploy-qa.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-echo "Hello, World!"
+echo "Decrypting SSH Keys"
+openssl enc -d -aes-256-cbc -in "Inner-Satisfaction.pem.enc" -out "Inner-Satisfaction.pem" -pass pass:$MY_SECRET_PASS
+
+echo "Changing Permissions for PEM Keys"
+sudo chmod 400 Inner-Satisfaction.pem.enc
+
+## Server Commands Start
+echo "SSH to Server"
+ssh -i Inner-Satisfaction.pem ubuntu@is.bismagreens.com
+
+echo "Exiting Server"
+logout
+## Server Commands End
+
+echo "Removing Keys"
+rm Inner-Satisfaction.pem
