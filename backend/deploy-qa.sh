3aa79b12b1c49b195fa7a0209f89df3cb08f3dd2
Mohsin Mansoor Kerai <mohsin.kerai@gmail.com>
2018-10-05 18:02:33 +0500

    Jar copying to remote server

--- backend/deploy-qa.sh
+++ backend/deploy-qa.sh
@@ -7,6 +7,9 @@ echo "Changing Permissions for PEM Keys"
 sudo chmod 400 Inner-Satisfaction.pem
 
 ## Server Commands Start
+echo "Copying Jar to Server"
+scp -i Inner-Satisfaction.pem target/inner-satisfaction-backend-0.0.1-SNAPSHOT.jar ubuntu@34.242.122.236:~/qa-build.jar
+
 echo "SSH to Server"
 ssh -i Inner-Satisfaction.pem ubuntu@34.242.122.236
 
