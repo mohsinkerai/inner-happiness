a0ff63bced10aa06cd4ae0edb44869e0a97a381d
Mohsin Mansoor Kerai <mohsin.kerai@gmail.com>
2018-10-05 17:55:34 +0500

    SSH Success + Killing Existing Process

--- backend/deploy-qa.sh
+++ backend/deploy-qa.sh
@@ -4,11 +4,14 @@ echo "Decrypting SSH Keys"
 openssl enc -d -aes-256-cbc -in "Inner-Satisfaction.pem.enc" -out "Inner-Satisfaction.pem" -pass pass:$SSH_KEY_PASSWORD
 
 echo "Changing Permissions for PEM Keys"
-sudo chmod 400 Inner-Satisfaction.pem.enc
+sudo chmod 400 Inner-Satisfaction.pem
 
 ## Server Commands Start
 echo "SSH to Server"
 ssh -i Inner-Satisfaction.pem ubuntu@34.242.122.236
+
+echo "Killing existing process"
+ps -ef | grep 'java' | grep '8080' | grep -v grep | awk '{print $2}'# | xargs -r kill -9
 
 echo "Exiting Server"
 logout
