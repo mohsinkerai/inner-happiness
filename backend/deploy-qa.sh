59d56e22379739e7898a401a99888392346cfe53
Mohsin Mansoor Kerai <mohsin.kerai@gmail.com>
2018-10-05 18:54:08 +0500

    Deploying -- se*rets open in market, need to echo it

--- backend/deploy-qa.sh
+++ backend/deploy-qa.sh
@@ -12,12 +12,16 @@ scp -i Inner-Satisfaction.pem target/inner-satisfaction-backend-0.0.1-SNAPSHOT.j
 
 echo "SSH to Server"
 ssh -i Inner-Satisfaction.pem ubuntu@34.242.122.236 <<EOF
-
-  echo "Killing existing process"
-  ps -ef | grep 'java' | grep '8080' | grep -v grep | awk '{print $2}'# | xargs -r kill -9
-
+ps -ef | grep 'java' | grep '8080' | grep -v grep | awk '{print \$2}' | xargs -r kill -9
+java -jar qa-build.jar --server.port=8080 --spring.datasource.password=U1kEPs --spring.datasource.host=is.csf40jim5d9x.eu-west-1.rds.amazonaws.com --spring.datasource.username=application-user --spring.datasource.db=inner_satisfaction -Xmx900m > startup_log.out 2>&1 &
 EOF
+
+#ssh -i Inner-Satisfaction.pem ubuntu@34.242.122.236 "ps -ef | grep 'java' | grep '8080' | grep -v grep | awk '{print \$2}'"
+## Backup Command
+##kill $(lsof -i:5000 -t)
 ## Server Commands End
+
+#ps -ef | grep 'java' | grep '8080' | grep -v grep | awk '{print $2}' | xargs -r kill -9
 
 echo "Removing Keys"
 rm Inner-Satisfaction.pem
