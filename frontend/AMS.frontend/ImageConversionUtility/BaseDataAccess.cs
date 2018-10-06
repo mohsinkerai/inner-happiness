88fca55bf3b80deeea84f1fe9f2b1acdac62a18c
Naveed tejani <naveed.tejani@outlook.com>
2018-10-06 16:14:28 +0500

    Added image conversion utility

---
+++ frontend/AMS.frontend/ImageConversionUtility/BaseDataAccess.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace ImageConversionUtility
+{
+    public class BaseDataAccess
+    {
+        protected string ConnectionString { get; set; }
+
+        public BaseDataAccess()
+        {
+        }
+
+        public BaseDataAccess(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        private SqlConnection GetConnection()
+        {
+            SqlConnection connection = new SqlConnection(ConnectionString);
+            if (connection.State != ConnectionState.Open)
+            {
+                connection.Open();
+            }
+
+            return connection;
+        }
+
+        protected DbCommand GetCommand(DbConnection connection, string commandText, CommandType commandType)
+        {
+            SqlCommand command = new SqlCommand(commandText, connection as SqlConnection)
+            {
+                CommandType = commandType
+            };
+            return command;
+        }
+
+        protected SqlParameter GetParameter(string parameter, object value)
+        {
+            SqlParameter parameterObject = new SqlParameter(parameter, value != null ? value : DBNull.Value)
+            {
+                Direction = ParameterDirection.Input
+            };
+            return parameterObject;
+        }
+
+        protected SqlParameter GetParameterOut(string parameter, SqlDbType type, object value = null, ParameterDirection parameterDirection = ParameterDirection.InputOutput)
+        {
+            SqlParameter parameterObject = new SqlParameter(parameter, type); ;
+
+            if (type == SqlDbType.NVarChar || type == SqlDbType.VarChar || type == SqlDbType.NText || type == SqlDbType.Text)
+            {
+                parameterObject.Size = -1;
+            }
+
+            parameterObject.Direction = parameterDirection;
+
+            if (value != null)
+            {
+                parameterObject.Value = value;
+            }
+            else
+            {
+                parameterObject.Value = DBNull.Value;
+            }
+
+            return parameterObject;
+        }
+
+        protected int ExecuteNonQuery(string procedureName, List<DbParameter> parameters, CommandType commandType = CommandType.StoredProcedure)
+        {
+            int returnValue = -1;
+
+            try
+            {
+                using (SqlConnection connection = GetConnection())
+                {
+                    DbCommand cmd = GetCommand(connection, procedureName, commandType);
+
+                    if (parameters != null && parameters.Count > 0)
+                    {
+                        cmd.Parameters.AddRange(parameters.ToArray());
+                    }
+
+                    returnValue = cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception)
+            {
+                //LogException("Failed to ExecuteNonQuery for " + procedureName, ex, parameters);
+                throw;
+            }
+
+            return returnValue;
+        }
+
+        protected object ExecuteScalar(string procedureName, List<SqlParameter> parameters)
+        {
+            object returnValue = null;
+
+            try
+            {
+                using (DbConnection connection = GetConnection())
+                {
+                    DbCommand cmd = GetCommand(connection, procedureName, CommandType.StoredProcedure);
+
+                    if (parameters != null && parameters.Count > 0)
+                    {
+                        cmd.Parameters.AddRange(parameters.ToArray());
+                    }
+
+                    returnValue = cmd.ExecuteScalar();
+                }
+            }
+            catch (Exception)
+            {
+                //LogException("Failed to ExecuteScalar for " + procedureName, ex, parameters);
+                throw;
+            }
+
+            return returnValue;
+        }
+
+        protected DbDataReader GetDataReader(string procedureName, List<DbParameter> parameters, CommandType commandType = CommandType.StoredProcedure)
+        {
+            DbDataReader ds;
+
+            try
+            {
+                DbConnection connection = GetConnection();
+                {
+                    DbCommand cmd = GetCommand(connection, procedureName, commandType);
+                    if (parameters != null && parameters.Count > 0)
+                    {
+                        cmd.Parameters.AddRange(parameters.ToArray());
+                    }
+
+                    ds = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                }
+            }
+            catch (Exception)
+            {
+                //LogException("Failed to GetDataReader for " + procedureName, ex, parameters);
+                throw;
+            }
+
+            return ds;
+        }
+    }
+}
---
+++ frontend/AMS.frontend/ImageConversionUtility/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace ImageConversionUtility
+{
+    internal class Program
+    {
+        #region Private Methods
+
+        private static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("config.json", false)
+                .Build();
+
+            var connectionString = configuration.GetSection("connectionString").Value;
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new ArgumentException("No connection string in config.json");
+            }
+
+            using (var conn = new SqlConnection(connectionString))
+            {
+                const string sql = "select dbo.ConvertImageToBase64(PersonId) from Ali_tblPerson where PhotoImage is not null";
+                using (var cmd = new SqlCommand(sql, conn))
+                {
+                    using (var adapter = new SqlDataAdapter(cmd))
+                    {
+                        var resultTable = new DataTable();
+                        adapter.Fill(resultTable);
+
+                        var writer = new StreamWriter("Queries.sql", false);
+
+                        foreach (DataRow row in resultTable.Rows)
+                        {
+                            writer.WriteLine(row[0] as string);
+                        }
+
+                        writer.Close();
+                    }
+                }
+            }
+        }
+
+        #endregion Private Methods
+    }
+}
