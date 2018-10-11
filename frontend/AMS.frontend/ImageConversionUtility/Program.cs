4f2cd32726d55e75165c2b4c9e53b82fd60ab80f
Naveed tejani <naveed.tejani@outlook.com>
2018-10-11 20:12:01 +0500

    Added id's migration code

--- frontend/AMS.frontend/ImageConversionUtility/Program.cs
+++ frontend/AMS.frontend/ImageConversionUtility/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Data;
-using System.Data.SqlClient;
 using System.IO;
 
 namespace ImageConversionUtility
@@ -24,21 +26,105 @@ namespace ImageConversionUtility
                 throw new ArgumentException("No connection string in config.json");
             }
 
-            using (var conn = new SqlConnection(connectionString))
+            //using (var conn = new SqlConnection(connectionString))
+            //{
+            //    const string sql = "select dbo.ConvertImageToBase64(PersonId) from Ali_tblPerson where PhotoImage is not null";
+            //    using (var cmd = new SqlCommand(sql, conn))
+            //    {
+            //        using (var adapter = new SqlDataAdapter(cmd))
+            //        {
+            //            var resultTable = new DataTable();
+            //            adapter.Fill(resultTable);
+
+            //            var writer = new StreamWriter("Queries.sql", false);
+
+            //            foreach (DataRow row in resultTable.Rows)
+            //            {
+            //                writer.WriteLine(row[0] as string);
+            //            }
+
+            //            writer.Close();
+            //        }
+            //    }
+            //}
+
+            using (var conn = new MySqlConnection(connectionString))
             {
-                const string sql = "select dbo.ConvertImageToBase64(PersonId) from Ali_tblPerson where PhotoImage is not null";
-                using (var cmd = new SqlCommand(sql, conn))
+                const string sql = "SELECT * FROM person";
+                using (var cmd = new MySqlCommand(sql, conn))
                 {
-                    using (var adapter = new SqlDataAdapter(cmd))
+                    cmd.CommandTimeout = 5000;
+                    using (var adapter = new MySqlDataAdapter(cmd))
                     {
                         var resultTable = new DataTable();
                         adapter.Fill(resultTable);
-
                         var writer = new StreamWriter("Queries.sql", false);
+                        writer.WriteLine("UPDATE country SET country.id = 10000 + country.id;");
+                        writer.WriteLine("UPDATE akdn_training SET akdn_training.id = 10000 + akdn_training.id;");
 
                         foreach (DataRow row in resultTable.Rows)
                         {
-                            writer.WriteLine(row[0] as string);
+                            var educations = row["educations"] as string;
+                            var akdnTrainings = row["akdn_trainings"] as string;
+                            var professionalTrainings = row["professional_trainings"] as string;
+                            var id = Convert.ToInt32(row["id"]);
+
+                            if (!string.IsNullOrWhiteSpace(educations) && educations.Contains("countryOfStudy"))
+                            {
+                                var educationsJson = JArray.Parse(educations);
+                                foreach (var education in educationsJson)
+                                {
+                                    if (Convert.ToInt32(education["countryOfStudy"]) != 0)
+                                    {
+                                        education["countryOfStudy"] =
+                                            10000 + Convert.ToInt32(education["countryOfStudy"]);
+                                    }
+                                }
+
+                                educations = educationsJson.ToString(Formatting.None);
+
+                                var updateSql =
+                                        $"UPDATE person SET person.educations = '{educations}' WHERE person.id = {id};";
+                                writer.WriteLine(updateSql);
+                            }
+
+                            if (!string.IsNullOrWhiteSpace(akdnTrainings) && akdnTrainings.Contains("countryOfTraining"))
+                            {
+                                var akdnTrainingsJson = JArray.Parse(akdnTrainings);
+                                foreach (var akdnTraining in akdnTrainingsJson)
+                                {
+                                    if (Convert.ToInt32(akdnTraining["countryOfTraining"]) != 0)
+                                    {
+                                        akdnTraining["countryOfTraining"] =
+                                            10000 + Convert.ToInt32(akdnTraining["countryOfTraining"]);
+                                    }
+                                }
+
+                                akdnTrainings = akdnTrainingsJson.ToString(Formatting.None);
+
+                                var updateSql =
+                                    $"UPDATE person SET person.akdn_trainings = '{akdnTrainings}' WHERE person.id = {id};";
+                                writer.WriteLine(updateSql);
+                            }
+
+                            if (!string.IsNullOrWhiteSpace(professionalTrainings) && professionalTrainings.Contains("countryOfTraining"))
+                            {
+                                var professionalTrainingsJson = JArray.Parse(professionalTrainings);
+                                foreach (var professionalTraining in professionalTrainingsJson)
+                                {
+                                    if (Convert.ToInt32(professionalTraining["countryOfTraining"]) != 0)
+                                    {
+                                        professionalTraining["countryOfTraining"] =
+                                            10000 + Convert.ToInt32(professionalTraining["countryOfTraining"]);
+                                    }
+                                }
+
+                                professionalTrainings = professionalTrainingsJson.ToString(Formatting.None);
+
+                                var updateSql =
+                                    $"UPDATE person SET person.professional_trainings = '{professionalTrainings}' WHERE person.id = {id};";
+                                writer.WriteLine(updateSql);
+                            }
                         }
 
                         writer.Close();
