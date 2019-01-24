195a838b968c3561bf3fa5ba8bc01dd7ac0236c5
Naveed tejani <naveed.tejani@outlook.com>
2019-01-24 16:07:51 +0500

    Fixed issue with jasper username and password

--- frontend/AMS.frontend/AMS.frontend.web/Areas/Operations/Controllers/ReportsController.cs
+++ frontend/AMS.frontend/AMS.frontend.web/Areas/Operations/Controllers/ReportsController.cs
@@ -31,7 +31,7 @@ namespace AMS.frontend.web.Areas.Operations.Controllers
         public IActionResult ThreePlusOne(int insitutionId, int cycleId)
         {
             using(var client = new WebClient()) {
-                client.Credentials = new NetworkCredential("japseradmin", "japseradmin");
+                client.Credentials = new NetworkCredential("jasperadmin", "jasperadmin");
                 using (var stream = new MemoryStream(client.DownloadData(
                     $"http://localhost:8081/jasperserver/rest_v2/reports/reports/Appointment/Three_Plus_One.pdf?institutionid={insitutionId}&cycleid={cycleId}"))
                 )
